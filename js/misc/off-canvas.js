a51fc4d84d6c33468c66243e68a841524d0a3d8f
Ted Bowman <ted.git@tedbow.com>
2017-06-09 16:23:46 -0400

    add js/misc/off-canvas.js

---
+++ js/misc/off-canvas.js
@@ -0,0 +1,20 @@
+
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.offCanvas.getContainer = function ($element) {
+    return $element;
+  };
+
+  Drupal.offCanvas.open = function (event, dialog, $element, settings) {
+    // Handle resizing and body displacement
+  };
+  Drupal.offCanvas.close = function (event, dialog, $element) {
+    // Handle body displacement
+  };
+  Drupal.offCanvas.render = function (event, dialog, $element, settings) {
+
+  };
+})(jQuery, Drupal);
